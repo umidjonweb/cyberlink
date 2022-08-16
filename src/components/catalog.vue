bfb06541db38bd50be7ec5e34e9b80b92defa8db
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2022-08-16 21:05:20 +0500

    catalog changed little

---
+++ src/components/catalog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="catalog">
+    <h2>КАТАЛОГ</h2>
+    <div class="columns"></div>
+    <div class="partners">
+      <img src="../assets/img/partners/dell.png" alt="" />
+      <img src="../assets/img/partners/cisco.png" alt="" />
+      <img src="../assets/img/partners/huawei.png" alt="" />
+      <img src="../assets/img/partners/fujitsu.png" alt="" />
+      <img src="../assets/img/partners/hpe.png" alt="" />
+      <img src="../assets/img/partners/lenovo.png" alt="" />
+      <img src="../assets/img/partners/westerndigital.png" alt="" />
+      <img src="../assets/img/partners/seagate.png" alt="" />
+      <img src="../assets/img/partners/hp.png" alt="" />
+      <img src="../assets/img/partners/microsoft.png" alt="" />
+      <img src="../assets/img/partners/jabra.png" alt="" />
+      <img src="../assets/img/partners/xerox.png" alt="" />
+      <img src="../assets/img/partners/canon.png" alt="" />
+      <img src="../assets/img/partners/tp-link.png" alt="" />
+      <img src="../assets/img/partners/MikroTik.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hello {
+  .bg {
+    color: red;
+  }
+}
+</style>
